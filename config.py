c7d5221e5a6daa3eb7af35c73fc8faf5f1afb1f5
mastemind234385 <mastermind191209@gmail.com>
2026-08-10 01:47:43 +0600

    Update TOKEN environment variable in config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-TOKEN = os.getenv("8729247713:AAHAAjMywZE8jZcYc4ySgOJgiLwzIa6JBog")
+TOKEN = os.getenv("8729247713:AAFeSZTVI-UuFHlfWCyRVd4nevuSfxJMMY0")
 
 WEATHER_API_KEY = "ed792d65d14e18e531b19d2be3bad9dc"
