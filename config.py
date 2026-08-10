7c77a1bf246b1b86bbcfcde85ed19069b65d4aee
mastemind234385 <mastermind191209@gmail.com>
2026-08-10 22:04:31 +0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-TOKEN = os.getenv("8729247713:AAFeSZTVI-UuFHlfWCyRVd4nevuSfxJMMY0")
+TOKEN = os.getenv("8729247713:AAFqfGF-B1PpsrwIPPM9KCVLFiGe9sVmrrk")
 
 WEATHER_API_KEY = "ed792d65d14e18e531b19d2be3bad9dc"
