dbf48e9160f4ad517a4d755f15bddac45d39e93b
mastemind234385 <mastermind191209@gmail.com>
2026-08-11 15:00:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-from telegram import Update
+
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 from telegram.ext import MessageHandler, filters
-from menu import menu
+
+from buttons.menu import menu, keyboard
 
 TOKEN = os.getenv("TOKEN")
 
@@ -11,8 +13,16 @@ print("TOKEN =", TOKEN)
 if not TOKEN:
     raise ValueError("TOKEN not found!")
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is working ✅")
+    await update.message.reply_text(
+        "👋 Welcome!\n\nনিচের Menu থেকে একটি অপশন নির্বাচন করুন।",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard,
+            resize_keyboard=True
+        )
+    )
+
 
 app = Application.builder().token(TOKEN).build()
 
